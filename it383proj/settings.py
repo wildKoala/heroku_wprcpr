1f4b7bc097b394799d86e58f80b010a93e56dd7c
Gordon <1513263739@mil>
2018-05-16 11:32:06 -0400

    Adds STATICFILES_DIRS

--- it383proj/settings.py
+++ it383proj/settings.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
